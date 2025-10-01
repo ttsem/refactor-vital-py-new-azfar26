1317704898620599f5bdb8a14a3828bb233b6420
Azfar Sabri <69881701+azfar26@users.noreply.github.com>
2025-10-01 21:37:07 +0800

    Rename class variables and internal method

--- core/vital_checks.py
+++ core/vital_checks.py
@@ -2,13 +2,13 @@ from core.base_vital_check import VitalCheck
 
 
 class TemperatureCheck(VitalCheck):
-    RANGES = {"child": (97, 100), "adult": (95, 102)}
+    TEMP_RANGES = {"child": (97, 100), "adult": (95, 102)}
 
-    def _get_range(self):
-        return self.RANGES["child"] if self.age < 12 else self.RANGES["adult"]
+    def _get_temp_range(self):
+        return self.TEMP_RANGES["child"] if self.age < 12 else self.TEMP_RANGES["adult"]
 
     def is_ok(self) -> bool:
-        low_temp, high_temp = self._get_range()
+        low_temp, high_temp = self._get_temp_range()
         return low_temp <= self.value <= high_temp
 
     def error_message(self) -> str:
@@ -16,13 +16,15 @@ class TemperatureCheck(VitalCheck):
 
 
 class PulseRateCheck(VitalCheck):
-    RANGES = {"child": (70, 120), "adult": (60, 100)}
+    PULSE_RANGES = {"child": (70, 120), "adult": (60, 100)}
 
-    def _get_range(self):
-        return self.RANGES["child"] if self.age < 12 else self.RANGES["adult"]
+    def _get_pulse_range(self):
+        return (
+            self.PULSE_RANGES["child"] if self.age < 12 else self.PULSE_RANGES["adult"]
+        )
 
     def is_ok(self) -> bool:
-        low_pulse, high_pulse = self._get_range()
+        low_pulse, high_pulse = self._get_pulse_range()
         return low_pulse <= self.value <= high_pulse
 
     def error_message(self) -> str:
@@ -31,13 +33,13 @@ class PulseRateCheck(VitalCheck):
 
 class Spo2Check(VitalCheck):
     # SPO2 rate is generally consistent across ages
-    RANGES = {"default": (90, 100)}
+    OXYGEN_RANGES = {"default": (90, 100)}
 
-    def _get_range(self):
-        return self.RANGES["default"]
+    def _get_oxygen_range(self):
+        return self.OXYGEN_RANGES["default"]
 
     def is_ok(self) -> bool:
-        low_oxygen, high_oxygen = self._get_range()
+        low_oxygen, high_oxygen = self._get_oxygen_range()
         return low_oxygen <= self.value <= high_oxygen
 
     def error_message(self) -> str:
@@ -46,7 +48,7 @@ class Spo2Check(VitalCheck):
 
 class BloodPressureCheck(VitalCheck):
     # Systolic and Diastolic as a tuple
-    RANGES = {
+    BP_RANGES = {
         "child": ((90, 110), (55, 75)),
         "adult": ((90, 120), (60, 80)),
     }
@@ -54,16 +56,13 @@ class BloodPressureCheck(VitalCheck):
     def __init__(self, systolic: int, diastolic: int, age: int):
         super().__init__((systolic, diastolic), age)
 
-    def _get_range(self):
-        return self.RANGES["child"] if self.age < 12 else self.RANGES["adult"]
+    def _get_bp_range(self):
+        return self.BP_RANGES["child"] if self.age < 12 else self.BP_RANGES["adult"]
 
     def is_ok(self) -> bool:
-        (sys_low, sys_high), (dia_low, dia_high) = self._get_range()
+        (sys_low, sys_high), (dia_low, dia_high) = self._get_bp_range()
         systolic, diastolic = self.value
-        return (
-            sys_low <= systolic <= sys_high
-            and dia_low <= diastolic <= dia_high
-        )
+        return sys_low <= systolic <= sys_high and dia_low <= diastolic <= dia_high
 
     def error_message(self) -> str:
         systolic, diastolic = self.value
